5c59bd5c8e37fee669c29b710b78b3c9b8d9347c
OkDoky <ie5630jk@naver.com>
2024-07-23 00:35:02 +0900

    max episode

--- __init__.py
+++ __init__.py
@@ -10,3 +10,10 @@ from sb3_contrib_drqn.drqn import DRQN
 __all__ = [
     "DRQN",
 ]
+from gymnasium.envs.registration import register
+
+register(
+     id="DataMap-v0",
+     entry_point="sb3_contrib_drqn.nav.data_map_env:CustomEnv",
+     max_episode_steps=600,  ## 60 sec
+)
