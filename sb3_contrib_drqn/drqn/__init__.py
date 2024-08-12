c7cd0ff669cd63b41dbf4b3e4c08b53a3772c20d
OkDoky <ie5630jk@naver.com>
2024-08-12 12:47:06 +0900

    dependencies

--- sb3_contrib_drqn/drqn/__init__.py
+++ sb3_contrib_drqn/drqn/__init__.py
@@ -1,5 +1,5 @@
 from sb3_contrib_drqn.drqn.policies import RecurrentDQNPolicy
 from sb3_contrib_drqn.drqn.drqn import DRQN
-from sb3_contrib_drqn.drqn.extractors import RNNStates, CustomRecurrentFeaturesExtractor
+from sb3_contrib_drqn.drqn.extractors import CustomRecurrentFeaturesExtractor
 
-__all__ = ["RecurrentDQNPolicy", "DRQN", "RNNStates", "CustomRecurrentFeaturesExtractor"]
+__all__ = ["RecurrentDQNPolicy", "DRQN", "CustomRecurrentFeaturesExtractor"]
